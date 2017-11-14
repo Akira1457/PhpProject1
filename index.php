c27729cdea0280125a07e3b275d6b8837daaca4c
Akira1457 <32962160+Akira1457@users.noreply.github.com>
2017-11-14 19:40:51 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php 
+$title = "Home";
+$content = '<img src="Images/mee_kari.jpg" class="imgleft" />
+<h3> Title 1</h3>
+<p> Ini adalah mee kari.</p>
+
+<img src="Images/nasi_lemak.jpg" class="imgright" />
+<h3> Title 2</h3>
+<p> Ini adalah nasi lemak.</p>';
+
+include 'Template.php';
+
+?>
+
